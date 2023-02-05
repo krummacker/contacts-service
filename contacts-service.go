ce86f940479421e6ba80d6e9b9323ba070bcf3f7
Dirk Krummacker <rasen_02cider@icloud.com>
2023-02-05 11:10:05 +0100

    Fix vet problem

--- contacts-service.go
+++ contacts-service.go
@@ -17,7 +17,7 @@ import (
 // Contact is the data structure for a person that we know.
 // All fields with the exception of the Id field are optional.
 type Contact struct {
-	Id       int64      `json: "id"                 db:"id"`
+	Id       int64      `json:"id"                 db:"id"`
 	Name     *string    `json:"name,omitempty"     db:"name"`
 	Phone    *string    `json:"phone,omitempty"    db:"phone"`
 	Birthday *time.Time `json:"birthday,omitempty" db:"birthday"`
