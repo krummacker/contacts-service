aad970fe41fde7b2f6b02a06abfb4ebf89a1edcc
Dirk Krummacker <rasen_02cider@icloud.com>
2023-04-02 18:08:02 +0200

    Also print error

--- cmd/wait-until-available/main.go
+++ cmd/wait-until-available/main.go
@@ -10,13 +10,18 @@ func main() {
 	totalWaitTime := 0
 	for {
 		res, err := http.Get("http://localhost:8080/contacts/")
-		fmt.Printf("Received status code: %d", res.StatusCode)
-		if err == nil && res.StatusCode == http.StatusNotFound {
+		if err != nil {
+			fmt.Printf("Received error: " + err.Error())
 			fmt.Println()
-			break
+		} else {
+			fmt.Printf("Received status code: %d", res.StatusCode)
+			fmt.Println()
+			if res.StatusCode == http.StatusNotFound {
+				break
+			}
 		}
 		totalWaitTime += 5
-		fmt.Printf(" - waiting %d more seconds...", totalWaitTime)
+		fmt.Printf("Waiting in total %d seconds...", totalWaitTime)
 		fmt.Println()
 		time.Sleep(5 * time.Second)
 	}
